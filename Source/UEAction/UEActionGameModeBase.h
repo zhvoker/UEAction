6f837b13b88e244a386ab6298d11bac79d899c19
zhvoker <982588451@qq.com>
2023-11-11 17:01:21 +0800

    first c

---
+++ Source/UEAction/UEActionGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UEActionGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEACTION_API AUEActionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
